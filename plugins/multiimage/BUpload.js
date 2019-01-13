11a987c1d6901b590e999154c15b58fc7ad85e88
yangjian <yangjian102621@gmail.com>
2019-01-13 22:35:15 +0800

    修复浏览器 HTML5 支持的判断代码

--- plugins/multiimage/BUpload.js
+++ plugins/multiimage/BUpload.js
@@ -7,7 +7,7 @@
 (function($) {
 
 	//判断浏览器是否支持html5
-	if ( !window.applicationCache )
+	if ( typeof FormData == "undefined" )
 		throw new Error("您当前的浏览器不支持HTML5,请先升级浏览器才能使用该上传插件!");
 
 	//image crop
